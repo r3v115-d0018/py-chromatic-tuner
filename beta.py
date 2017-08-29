438cb1ed4656a4f57a0fba4b2b9e9f827f9e2661
Ilias Tsatiris <eliastsatiris@gmail.com>
2017-08-29 17:46:26 +0300

    Add colors to display

--- beta.py
+++ beta.py
@@ -27,6 +27,15 @@ def findnote(freq):
     n =int(h % 12)
     return (name[n], octave)
 
+def colorize(string, position):
+    if position == 3:
+        return '\033[31m' + string + '\033[0m'
+    elif position == 2:
+        return '\033[33m' + string + '\033[0m'
+    elif position == 1:
+        return '\033[93m' + string + '\033[0m'
+    else:
+        return '\033[32m' + string + '\033[0m'
 
 #PyAudio stream object.
 p = pyaudio.PyAudio()
@@ -47,17 +56,20 @@ pDetection.set_silence(-10)
 prevpitch = 0
 
 while True:
-
     data = stream.read(1024)
     samples = num.fromstring(data,
         dtype=aubio.float_type)
     pitch = pDetection(samples)[0]
+
     # Compute the energy (volume) of the
     # current frame.
     # Format the volume output so that at most
     # it has six decimal numbers.
 
+    #pitch = 427.76
+
     if pitch!= 0 and abs(prevpitch - pitch) > 1.5 :
+        #prevpitch = 0
         prevpitch = pitch
 	note,octave = findnote(pitch)
     	output = "Pitch:" + str(pitch) + "Hz Note:" + note + str(octave) + "            "
@@ -73,22 +85,19 @@ while True:
         else:
             margin = 0.5 * (pitch - pitch * pow(2,-1.0/12))
         
-        
-        
         errorper = abs(error) / margin
         
-        
         if errorper <= tolerance:
-            print ("  .  .  .  ^  .  .  .  " + output + '\r',end = '')
+            print ("  .  .  ." + colorize("  ^  ", 0) + ".  .  .  " + output + '\r',end = '')
         else:
             if error > 0:
                 pre = "  .  .  .  "
                 pos = int(math.ceil(3*errorper))
-                post = (pos - 1) * "  ." + "  <" + (3 - pos) * "  ."
+                post = (pos - 1) * "  ." + colorize("  <", pos) + (3 - pos) * "  ."
             else:
                 post = "  .  .  ."
                 pos = 3 - int(math.ceil(3*errorper)) + 1
-                pre = "  " + (pos - 1) * ".  " + ">  " + (3 - pos) * ".  "
+                pre = "  " + (pos - 1) * ".  " + colorize(">  ", 4 - pos) + (3 - pos) * ".  "
         
             print (pre + "|" + post + "  " + output + '\r', end = '')
 	sys.stdout.flush()
